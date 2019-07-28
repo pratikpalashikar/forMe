0a456ff169ed12a9e9684994da0ae160c05d60fc
PratikPalashikar <pratikpalashikar@gmail.com>
2019-07-28 10:31:59 -0400

    initializing the spring boot application

--- src/main/java/com/techmisal/forme/ForMe.java
+++ src/main/java/com/techmisal/forme/ForMe.java
@@ -1,8 +1,15 @@
 package com.techmisal.forme;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ForMe {
 
+  //Indicates to boostrap the application from
   public static void main(String[] args) {
+
+    SpringApplication.run(ForMe.class, args);
 
   }
 
